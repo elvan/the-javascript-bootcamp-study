5782af86e190fd6c8bfbbacddfdc534072bf17ea
Elvan H <elvanhd@gmail.com>
2021-01-24 23:31:45 +0700

    jsconfig setup

--- timer/index.js
+++ timer/index.js
@@ -1,14 +1,21 @@
+// @ts-check
+
 class Timer {
+  /**
+   * @param {HTMLElement} durationInput
+   * @param {HTMLElement} startButton
+   * @param {HTMLElement} pauseButton
+   */
   constructor(durationInput, startButton, pauseButton) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener('click', this.start());
+    this.startButton.addEventListener('click', this.start);
   }
 
   start() {
-    console.log('Time to start the timer!');
+    console.log(this);
   }
 }
 
@@ -17,3 +24,4 @@ const startButton = document.getElementById('start');
 const pauseButton = document.getElementById('pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
+timer.start();
