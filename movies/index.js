feaaa5c616ced9982aea5d91e2dbc4a33aef5501
Elvan H <elvanhd@gmail.com>
2021-01-25 14:16:23 +0700

    fetching movie data

--- movies/index.js
+++ movies/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apiKey: '2e95ca6d',
+      s: 'avengers',
+    },
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
