946f2d897ab8732e7674fc2ca84a03db0c6b76cd
Elvan H <elvanhd@gmail.com>
2021-01-26 10:07:19 +0700

    repairing references

--- movies/index.js
+++ movies/index.js
@@ -17,7 +17,7 @@ const root = document.querySelector('.autocomplete');
 root.innerHTML = `
   <label><b>Search For a Movie</b></label>
   <input class="input" />
-  <div class="dropdown>
+  <div class="dropdown">
     <div class="dropdown-menu">
       <div class="dropdown-content results"></div>
     </div>
@@ -31,15 +31,18 @@ const resultWrapper = document.querySelector('.results');
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
-  for (let movie of movies) {
-    const div = document.createElement('div');
+  dropdown.classList.add('is-active');
 
-    div.innerHTML = `
+  for (let movie of movies) {
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+      ${movie.Title}
     `;
 
-    document.querySelector('#target').appendChild(div);
+    resultWrapper.appendChild(option);
   }
 };
 
