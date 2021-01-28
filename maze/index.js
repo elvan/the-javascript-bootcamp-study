7cba5c3a5637783f3884ec5ad8ed0841f3b7eb1f
Elvan H <elvanhd@gmail.com>
2021-01-28 13:08:53 +0700

    verticals and horizontals

--- maze/index.js
+++ maze/index.js
@@ -33,4 +33,14 @@ const grid = Array(3)
   .fill(null)
   .map(() => Array(3).fill(false));
 
+const verticals = Array(3)
+  .fill(null)
+  .map(() => Array(2).fill(false));
+
+const horizontals = Array(2)
+  .fill(null)
+  .map(() => Array(3).fill(true));
+
 console.log(grid);
+console.log(verticals);
+console.log(horizontals);
