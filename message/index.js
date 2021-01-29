6032d49eb2b47741226387b6eeb409800ff7045d
Elvan H <elvanhd@gmail.com>
2021-01-29 20:01:18 +0700

    encoding the entered string

--- message/index.js
+++ message/index.js
@@ -1,7 +1,9 @@
-document.querySelector('form')?.addEventListener('submit', (event) => {
+// @ts-nocheck
+document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const input = document.querySelector('input');
+  const input = document.querySelector('#message');
+  const encrypted = btoa(input.value);
 
-  console.log(input?.value);
+  document.querySelector('#link-input').value = encrypted;
 });
