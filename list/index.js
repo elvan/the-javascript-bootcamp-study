15fc325462a0885df3c9d9a1f937a535d29aba7c
Elvan H <elvanhd@gmail.com>
2021-01-30 19:31:17 +0700

    using Promise

--- list/index.js
+++ list/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 // Method #2
-const lstat = util.promisify(fs.lstat);
+// const lstat = util.promisify(fs.lstat);
 
 // Method #3
 // const { lstat } = fs.promises;
@@ -26,14 +26,14 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 });
 
 // Method #1
-// const lstat = (filename) => {
-//   return new Promise((resolve, reject) => {
-//     fs.lstat(filename, (err, stats) => {
-//       if (err) {
-//         reject(err);
-//       }
+const lstat = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        reject(err);
+      }
 
-//       resolve(stats);
-//     });
-//   });
-// };
+      resolve(stats);
+    });
+  });
+};
