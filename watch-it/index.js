9f0ec3d80c76b998789b53e895055a789191fd72
Elvan H <elvanhd@gmail.com>
2021-01-31 14:33:03 +0700

    watching files with chokidar

--- watch-it/index.js
+++ watch-it/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Test');
+const chokidar = require('chokidar');
+
+chokidar
+  .watch('.')
+  .on('add', () => console.log('file added'))
+  .on('change', () => console.log('file changed'))
+  .on('unlink', () => console.log('file unlinked'));
