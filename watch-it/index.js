738d62d10aa601098c825b885e8f84da861de044
Elvan H <elvanhd@gmail.com>
2021-01-31 16:03:00 +0700

    handling cli tools with caporal

--- watch-it/index.js
+++ watch-it/index.js
@@ -4,12 +4,21 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 
-const start = debounce(() => {
-  console.log('starting users program');
-}, 100);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    console.log(args);
+  });
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('file changed'))
-  .on('unlink', () => console.log('file unlinked'));
+program.parse(process.argv);
+
+// const start = debounce(() => {
+//   console.log('starting users program');
+// }, 100);
+
+// chokidar
+//   .watch('.')
+//   .on('add', start)
+//   .on('change', () => console.log('file changed'))
+//   .on('unlink', () => console.log('file unlinked'));
