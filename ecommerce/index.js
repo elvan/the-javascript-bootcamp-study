b3f0604f87a31c799c17a9ba4c48fd6512f8e2a8
Elvan H <elvanhd@gmail.com>
2021-02-01 09:47:45 +0700

    creating a port variable

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
