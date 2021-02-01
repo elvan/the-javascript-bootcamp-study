f7a72c13d552cf213142da7dda72de4c7008bc6a
Elvan H <elvanhd@gmail.com>
2021-02-01 15:42:16 +0700

    middlewares in express

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', (data) => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
 app.get('/', (req, res) => {
   res.send(`
     <div>
-      <form method="post">
+      <form method="POST">
         <input name="email" type="text" placeholder="email" />
         <input name="password" type="password" placeholder="password" />
         <input name="password_confirmation" type="password" placeholder="password confirmation" />
@@ -15,17 +33,8 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  req.on('data', (data) => {
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
   res.send('Form submitted!');
 });
 
