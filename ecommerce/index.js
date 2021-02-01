d558d30136c1471e5156c39fdf48e33c07afbeec
Elvan H <elvanhd@gmail.com>
2021-02-01 09:52:59 +0700

    displaying simple html

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hi there!');
+  res.send(`
+    <div>
+      <form>
+        <input name="email" type="text" placeholder="email" />
+        <input name="password" type="password" placeholder="password" />
+        <input name="password_confirmation" type="password" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
 });
 
 app.listen(port, () => {
