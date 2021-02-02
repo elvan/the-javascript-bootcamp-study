43fa00c937b4d3df2877e5369d2dcda3bbc9e1a6
Elvan H <elvanhd@gmail.com>
2021-02-02 09:27:22 +0700

    signup validation logic

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users.repository');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,16 +13,26 @@ app.get('/', (req, res) => {
       <form method="POST">
         <input name="email" type="text" placeholder="email" />
         <input name="password" type="password" placeholder="password" />
-        <input name="password_confirmation" type="password" placeholder="password confirmation" />
+        <input name="passwordConfirmation" type="password" placeholder="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('Form submitted!');
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email: email });
+
+  if (existingUser) {
+    return res.send('Email in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
+  res.send('Account Created!');
 });
 
 app.listen(port, () => {
