fafa34e1d4cc6dea6e430fac1e761d93c9dd1746
Elvan H <elvanhd@gmail.com>
2021-02-03 14:50:10 +0700

    install express-validator

--- ecommerce/routes/admin/auth.routes.js
+++ ecommerce/routes/admin/auth.routes.js
@@ -1,34 +1,40 @@
 // @ts-nocheck
 
 const express = require('express');
-const router = express.Router();
+const { check } = require('express-validator');
 
 const usersRepo = require('../../repositories/users.repository');
 const signinView = require('../../views/admin/auth/signin.view');
 const signupView = require('../../views/admin/auth/signup.view');
 
+const router = express.Router();
+
 router.get('/signup', (req, res) => {
   res.send(signupView({ req: req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email: email });
+router.post(
+  '/signup',
+  [check('email'), check('password'), check('passwordConfirmation')],
+  async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+    const existingUser = await usersRepo.getOneBy({ email: email });
 
-  if (existingUser) {
-    return res.send('Email in use');
+    if (existingUser) {
+      return res.send('Email in use');
+    }
+
+    if (password !== passwordConfirmation) {
+      return res.send('Passwords must match');
+    }
+
+    const user = await usersRepo.create({ email: email, password: password });
+
+    req.session.userID = user.id;
+
+    res.send('Account Created!');
   }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-
-  const user = await usersRepo.create({ email: email, password: password });
-
-  req.session.userID = user.id;
-
-  res.send('Account Created!');
-});
+);
 
 router.get('/signin', (req, res) => {
   res.send(signinView());
