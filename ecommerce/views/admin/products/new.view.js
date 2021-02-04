6223c6b5b87c95e7ba7c1281ce2bfad8771c5613
Elvan H <elvanhd@gmail.com>
2021-02-04 09:26:46 +0700

    building the product creation form

---
+++ ecommerce/views/admin/products/new.view.js
@@ -0,0 +1,23 @@
+const layout = require('../admin.layout');
+const getError = require('../../error.helper');
+
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+    <form method="POST">
+      <div>
+        <input name="title" placeholder="Title" />
+      </div>
+      <div>
+        <input name="price" placeholder="Price" />
+      </div>
+      <div>
+        <input name="image" type="file" />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+    `,
+  });
+};
