bcfac573bdb2426a95d5cee72a654ebd714d70f0
Elvan H <elvanhd@gmail.com>
2021-02-05 14:56:42 +0700

    the multipart form-data

--- ecommerce/views/admin/products/new.view.js
+++ ecommerce/views/admin/products/new.view.js
@@ -4,7 +4,7 @@ const getError = require('../../error.helper');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <form method="POST">
+    <form method="POST" enctype="multipart/form-data">
       <div>
         <input name="title" placeholder="Title" />
       </div>
