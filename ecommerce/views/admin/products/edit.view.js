1700d37fd5b84a838cb0925a14d70b9584e01567
Elvan H <elvanhd@gmail.com>
2021-02-06 21:13:00 +0700

    edit form template

--- ecommerce/views/admin/products/edit.view.js
+++ ecommerce/views/admin/products/edit.view.js
@@ -4,21 +4,29 @@ const getError = require('../../error.helper');
 module.exports = ({ product, errors }) => {
   return layout({
     content: `
-      <form method="POST">
-        <div>
-          <input class="input" name="title" value="${product.title}">
-          ${getError(errors, 'title')}
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Edit a Product</h1>
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input value="${product.title}" class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            <div class="field">
+              <label class="label">Price</label>
+              <input value="${product.price}" class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            <div class="field">
+              <label class="label">Image</label>
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Edit</button>
+          </form>
         </div>
-        <div>
-          <input class="input" name="price" value="${product.price}">
-          ${getError(errors, 'price')}
-        </div>
-        <div>
-          <input type="file" name="image" />
-        </div>
-        <br />
-        <button>Save</button>
-      </form>
+      </div>
     `,
   });
 };
