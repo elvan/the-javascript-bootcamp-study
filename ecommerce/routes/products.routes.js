a1bbd9298eb79aa0e7510a3ce83a7b5fb1b6b114
Elvan H <elvanhd@gmail.com>
2021-02-07 13:23:06 +0700

    products index

--- ecommerce/routes/products.routes.js
+++ ecommerce/routes/products.routes.js
@@ -2,10 +2,14 @@
 
 const express = require('express');
 
+const productsRepo = require('../repositories/products.repository');
+const productsIndexTemplate = require('../views/products/index.template');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.send('Products');
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
 });
 
 module.exports = router;
