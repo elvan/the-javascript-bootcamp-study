8e158d3f9ed98cde862bb6a009d4df4d90b71864
Elvan H <elvanhd@gmail.com>
2021-02-07 20:24:45 +0700

    creating a cart

--- ecommerce/routes/carts.routes.js
+++ ecommerce/routes/carts.routes.js
@@ -1,9 +1,21 @@
+// @ts-nocheck
+
 const express = require('express');
+
+const cartsRepo = require('../repositories/carts.repository');
 
 const router = express.Router();
 
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productID);
+router.post('/cart/products', async (req, res) => {
+  let cart;
+  if (!req.session.cartID) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartID = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartID);
+  }
+
+  console.log(cart);
 
   res.send('Product added to cart');
 });
