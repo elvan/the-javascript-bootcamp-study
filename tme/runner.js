eeba62666d690a6a9160fbcb94b129fcb3474069
Elvan H <elvanhd@gmail.com>
2021-02-10 00:35:31 +0700

    adding basic reporting

--- tme/runner.js
+++ tme/runner.js
@@ -18,10 +18,22 @@ class Runner {
 
       global.it = (desc, fn) => {
         beforeEaches.forEach(func => func());
-        fn();
+
+        try {
+          fn();
+          console.log(`OK - ${desc}`);
+        } catch (error) {
+          console.log(`X - ${desc}`);
+          console.log('>>', error.message);
+        }
       };
 
-      require(file.name);
+      try {
+        require(file.name);
+      } catch (error) {
+        console.log(error);
+      }
+
     }
   }
 
