fcb2cc444c7227dfd66acfb229440ceaf027d194
Elvan H <elvanhd@gmail.com>
2021-02-11 12:54:46 +0700

    issue: console log is not working

--- tme/sample-web-project/test/app.test.js
+++ tme/sample-web-project/test/app.test.js
@@ -9,3 +9,18 @@ it('has a text input', async () => {
 
   assert(input);
 });
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'test@example.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h1 = dom.window.document.querySelector('h1');
+
+  assert(h1);
+  console.log('Content of h1', h1.innerHTML);
+});
