c1c2a2634bf1245a2ae763692e58fbe42baf8afa
Elvan H <elvanhd@gmail.com>
2021-02-11 07:36:15 +0700

    a sample web project

---
+++ tme/sample-web-project/index.js
@@ -0,0 +1,15 @@
+// @ts-nocheck
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { value } = document.querySelector('input');
+
+  const header = document.querySelector('h1');
+
+  if (value.includes('@')) {
+    header.innerHTML = 'Looks good';
+  } else {
+    header.innerHTML = 'Invalid email';
+  }
+});
